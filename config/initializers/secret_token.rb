990a1c2413cce8a3566e126ca5dffeac52b510a4
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-17 16:56:05 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CultureOverflow::Application.config.secret_token = '99123e3954e79e02febf7160b22632d5e4f26c3b9a18dbc47448d0caa551b33c8e211c35d727101babb68d20795d561ee1c8680b6b258fe3ed487dd0d60dd914'
