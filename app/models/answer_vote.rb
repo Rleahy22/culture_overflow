3380179ba640da543affd4317892aa14e3a886ea
Rick Thomas <rickthomas1980@gmail.com>
2013-07-19 09:57:56 -0500

    added associations for 6 models

--- app/models/answer_vote.rb
+++ app/models/answer_vote.rb
@@ -1,3 +1,8 @@
 class AnswerVote < ActiveRecord::Base
   attr_accessible :answer_id, :user_id
+
+  validates :user_id, uniqueness: true, scope: :answer_id
+
+  belongs_to :user
+  belongs_to :answer
 end
