8e01dadea412fd43df00eae3ba84d0f3d61eb067
Rick Thomas <rickthomas1980@gmail.com>
2013-07-21 19:55:47 -0500

    added question voting

--- app/controllers/question_votes_controller.rb
+++ app/controllers/question_votes_controller.rb
@@ -1,3 +1,19 @@
 class QuestionVotesController < ApplicationController
 
+  def create
+    p params
+    p "asterisk"
+    @user = User.find(session[:user_id])
+    @question = Question.find(params[:question_vote][:question_id])
+    if @user.question_votes.where(question_id: @question.id).count <= 0
+      QuestionVote.create(user_id: @user.id, question_id: @question.id, upordown: params[:question_vote][:upordown])
+    # elsif
+    end
+    redirect_to @question
+  end
+
+  def index
+    redirect_to '/'
+  end
+
 end
